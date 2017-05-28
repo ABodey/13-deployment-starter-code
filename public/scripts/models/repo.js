f3479e8dbcdd2faafb32fb396cc9e852afe8457a
Andrew Bodey <a.s.bodey@gmail.com>
2017-05-28 15:12:26 -0700

    fix typo

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -9,7 +9,7 @@
     //   url: `https://api.github.com/user/repos`,
 
     // TODONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route. Since the new proxy route is handling our token for us, we do not need it here, which is why we can refactor to to the simpler $.get method.
-    $.get('/github/users/repos', function (data) {
+    $.get('/github/user/repos', function (data) {
       repos.all = data;
       callback();
     });
